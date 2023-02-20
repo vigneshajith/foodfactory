50c5c27f36f9ed35ad478809306b18e46560ddd7
vigneshajith <vigneshajith152@gmail.com>
2023-02-20 22:25:37 +0530

    foodService was injected in home.c.ts

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { FoodService } from 'src/app/services/food.service';
+import { Food } from 'src/app/shared/model/food';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  foods:Food[] = []
+  constructor(private foodService: FoodService) {
+    this.foods = foodService.getAll()
+   }
+  ngOnInit(): void {
+    
+  }
 }
